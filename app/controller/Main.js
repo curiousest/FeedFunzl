16c07790a6760e2fab5976e781248f0ff1f3be64
Doug Hindson <doug.hindson@bearingpoint.com>
2012-11-15 14:49:33 +0000

    Single panel minor fix: initialize DetailNavController.singlePanel properly

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -33,6 +33,7 @@ Ext.define('Funzl.controller.Main', {
             this.singlePaned = true;
         }
         this.detailNavController = this.getApplication().getController('DetailNavController');
+        this.detailNavController.singlePaned = this.singlePaned;
         
         if (!this.singlePaned)
             this.menuNavController = this.getApplication().getController('MenuNavController');
